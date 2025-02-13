770bba6356a6020aad52f396d8db7f96c0c219db
Emmeline Thomsen <emmelinethomsen@MacBook-Pro-297.local>
2025-02-12 23:03:02 -0800

    final

--- madlibs/v2-final/script.js
+++ madlibs/v2-final/script.js
@@ -3,7 +3,7 @@
     console.log('reading js');
 
 
-    const myForm=document.querySelector('#myform');
+    const myForm=document.querySelector('form');
     const errorText=document.querySelector('#errorText');
     const madLib=document.querySelector('#madlib');
 
@@ -24,63 +24,63 @@
 
         let myText;
 
-        if(time == ''){
+        if(time == ""){
             myText = "please provide a time of day";
-            document.querySelector('#time').focus();
+            document.querySelector('#time').focus;
             errorText.innerHTML = myText;
         }
-        else if(object1 == ''){
+        else if(object1 == ""){
             myText = "please provide an object";
-            document.querySelector('#object1').focus();
+            document.querySelector('#object1').focus;
             errorText.innerHTML = myText;
         }
-        else if(object2 == ''){
+        else if(object2 == ""){
             myText = "please provide an object";
-            document.querySelector('#object2').focus();
+            document.querySelector('#object2').focus;
             errorText.innerHTML = myText;
         }
-        else if(emotion == ''){
+        else if(emotion == ""){
             myText = "please provide an emotion";
-            document.querySelector('#emotion').focus();
+            document.querySelector('#emotion').focus;
             errorText.innerHTML = myText;
         }
-        else if(object3 == ''){
+        else if(object3 == ""){
             myText = "please provide an object";
-            document.querySelector('#object3').focus();
+            document.querySelector('#object3').focus;
             errorText.innerHTML = myText;
         }
-        else if(adjective == ''){
+        else if(adjective == ""){
             myText = "please provide an adjective";
-            document.querySelector('#adjective').focus();
+            document.querySelector('#adjective').focus;
             errorText.innerHTML = myText;
         }
-        else if(verbing == ''){
+        else if(verbing == ""){
             myText = "please provide a verb ending in -ing";
-            document.querySelector('#verbing').focus();
+            document.querySelector('#verbing').focus;
             errorText.innerHTML = myText;
         }
-        else if(verb == ''){
+        else if(verb == ""){
             myText = "please provide a verb";
-            document.querySelector('#verb').focus();
+            document.querySelector('#verb').focus;
             errorText.innerHTML = myText;
         }
-        else if(animal == ''){
+        else if(animal == ""){
             myText = "please provide an animal";
-            document.querySelector('#animal').focus();
+            document.querySelector('#animal').focus;
             errorText.innerHTML = myText;
         }
-        else if(clothing == ''){
+        else if(clothing == ""){
             myText = "please provide an article of clothing";
-            document.querySelector('#clothing').focus();
+            document.querySelector('#clothing').focus;
             errorText.innerHTML = myText;
         }
 
         else{
-            myText = `On the 9th of January, 2025 at ${time} the residents at 1020B began the Great War of the Red Door Duplexes. They plotted and planned, and ultimately decided to swap our ${object1} with their ${object2}. In the following morning the 1020A residents awoke to see their updated house and were overcome with ${emotion}. 
+            myText = `Dear Resident, due to the following events we will be issuing a notice of eviction. One week ago, at ${time} the residents at 1020B began the Great War of the Red Door Duplexes when they swapped the ${object1} with their ${object2}. In the following morning the 1020A residents awoke to see their updated house and were overcome with feeling ${emotion}. 
 
-            Then on January 11th, 2025, duplex A responded to the actions of their neighbors since their ${object3} was too ${adjective}. With some ${verbing} around, the roommates of duplex A succeeded to ${verb} duplex B’s ${object2}
+            Later, Duplex A responded to their neighbors since their ${object3} was too ${adjective}. With some ${verbing} around, Duplex A succeeded to ${verb} Duplex B’s ${object2}
 
-            The final straw occurred on January 13, 2025 when the war reached a new level and 1020B unleashed a surprise: a ${animal} wearing a ${clothing}. They rampaged through the duplex, leaving destruction in their wake.`;
+            The final straw occurred when 1020B unleashed a surprise: a ${animal} wearing a ${clothing}. They rampaged through the duplex, leaving destruction in their wake. For these reasons, the residents of 1020A and 1020B are unable to continue their lease.`;
             document.querySelector('#time').value= "";
             document.querySelector('#object1').value= "";
             document.querySelector('#object2').value= "";
@@ -106,6 +106,7 @@
                     document.querySelector('#overlay').className = 'hidden';
                 }
             });
+
         }
 
 
